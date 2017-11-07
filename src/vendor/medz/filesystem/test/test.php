2a3e38098e802a9cd6f328f33eea74dbb7224372
shichaopeng <shichaopeng@jd.com>
2017-11-07 10:08:16 +0800

    ignore vendor

--- src/vendor/medz/filesystem/test/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include dirname(__FILE__) . '/../vendor/autoload.php';
-
-use Medz\Component\Filesystem\Filesystem;
-
-Filesystem::mirror('./1', './2');
--- src/vendor/medz/vendor-pack-pinyin/example.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once 'Pinyin.php';
-echo Pinyin::getPinyin("早上好");
-echo Pinyin::getShortPinyin("早上好");
